0496d93ac4beb8316a77d0104df911dc51ab1323
Igor <gorbov18@gmail.com>
2019-07-14 14:16:19 +0400

    Create user route

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
+app.use(cookieParser());
+
+app.use('/api/v1/users', userRouter);
 app.get('/', (req, res) => res.send('Coding hello!'));
 
 module.exports = app;
