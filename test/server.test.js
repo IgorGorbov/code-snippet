a72b1441a0af7a6ea5402ae036217f5f7639377f
Igor <gorbov18@gmail.com>
2019-07-15 19:37:09 +0400

    Test: login

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
+const fixtures = require('sequelize-fixtures');
+
 const app = require('../app');
-const { sequelize } = require('../models');
+const { sequelize, models } = require('../models');
 
 beforeEach(async () => {
   await sequelize.sync({ force: true });
@@ -20,12 +22,12 @@ describe('GET /', () => {
 });
 
 describe('POST /api/users/signup', () => {
-  test('It should save cookies', () => {
-    const testUser = { username: 'test', password: '1234' };
+  const testUser = { username: 'test', password: '1234', confirmPassword: '1234' };
 
+  test('It should save cookies', () => {
     return request(app)
       .post('/api/v1/users/signup')
-      .send({ username: testUser.username, password: testUser.password })
+      .send(testUser)
       .expect(201)
       .then(response => {
         expect(response.headers['set-cookie']).toBeTruthy();
@@ -34,11 +36,9 @@ describe('POST /api/users/signup', () => {
   });
 
   test('It should return new user', () => {
-    const testUser = { username: 'test', password: '1234' };
-
     return request(app)
       .post('/api/v1/users/signup')
-      .send({ username: testUser.username, password: testUser.password })
+      .send(testUser)
       .expect(201)
       .then(response => {
         const { status, token, user } = response.body;
@@ -53,11 +53,50 @@ describe('POST /api/users/signup', () => {
   });
 
   test('It should response with 400', () => {
-    const testUser = { username: 't', password: '1234' };
-
+    const notValidUser = { username: 't', password: '1234', confirmPassword: '12' };
     return request(app)
       .post('/api/v1/users/signup')
-      .send({ username: testUser.username, password: testUser.password })
+      .send(notValidUser)
       .expect(400);
+  });
+});
+
+describe('POST /api/users/login', () => {
+  const testUser = { id: 1, username: 'test', password: '1234', isAdmin: false };
+  const fixures = {
+    model: 'User',
+    data: { ...testUser }
+  };
+
+  beforeEach(async () => {
+    await fixtures.loadFixture(fixures, models);
+  });
+
+  test('It should save cookies', async () => {
+    return request(app)
+      .post('/api/v1/users/login')
+      .send({ username: testUser.username, password: testUser.password })
+      .expect(200)
+      .then(response => {
+        expect(response.headers['set-cookie']).toBeTruthy();
+        expect(response.headers['set-cookie'][0]).toContain('jwt');
+      });
+  });
+
+  test('It should return user', async () => {
+    return request(app)
+      .post('/api/v1/users/login')
+      .send({ username: testUser.username, password: testUser.password })
+      .expect(200)
+      .then(response => {
+        const { status, token, user } = response.body;
+
+        expect(status).toBe('success');
+        expect(token).toHaveLength(137);
+        expect(user.id).not.toBeNaN();
+        expect(user.username).toBe(testUser.username);
+        expect(user.password).toBeUndefined();
+        expect(user.isAdmin).toBeFalsy();
+      });
   });
 });
