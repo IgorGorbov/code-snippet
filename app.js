2928dd96f7a8a2963242c85a9e7b343a2529f4d0
Igor <gorbov18@gmail.com>
2019-07-17 16:52:25 +0400

    Create Category route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const userRouter = require('./routes/userRoutes');
+const categoryRouter = require('./routes/categoryRoutes');
 
 const app = express();
 
@@ -12,6 +13,8 @@ if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 app.use(cookieParser());
 
 app.use('/api/v1/users', bodyParser.json(), userRouter);
+app.use('/api/v1/categories', bodyParser.json(), categoryRouter);
+
 app.get('/', (req, res) => res.send('Coding hello!'));
 
 module.exports = app;
---
+++ controllers/categoryController.js
@@ -0,0 +1,31 @@
+const pick = require('lodash/pick');
+const Joi = require('joi');
+const { FactoryModels } = require('../models');
+
+const validate = req => {
+  const schema = {
+    name: Joi.string()
+      .min(1)
+      .max(40)
+      .required()
+  };
+
+  return Joi.validate(req, schema);
+};
+
+exports.create = async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  try {
+    const category = await FactoryModels('Category').create(req.body);
+
+    return res.status(201).json({
+      status: 'success',
+      category: pick(category, ['id', 'name'])
+    });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(400).send(err.message);
+  }
+};
