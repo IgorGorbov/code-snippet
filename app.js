b421a0f80380a9b143ac8416089a690e99bc83a7
Igor <gorbov18@gmail.com>
2019-07-24 20:45:35 +0400

    Add secure middlewares and CORS

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
+const compression = require('compression');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 
 const winston = require('./config/winston');
 
@@ -11,12 +17,24 @@ const categoryRouter = require('./routes/categoryRoutes');
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(xss());
+app.use(compression());
+app.use(cookieParser());
+app.use(hpp({ whitelist: ['id'] }));
+
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 else app.use(morgan('combined', { stream: winston.stream }));
 
-app.use(cookieParser());
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
 
 app.use('/', bodyParser.json(), authRoutes);
+app.use('/api', limiter);
 app.use('/api/v1/snippets', bodyParser.json(), snippetRouter);
 app.use('/api/v1/categories', bodyParser.json(), categoryRouter);
 
