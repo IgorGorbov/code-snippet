11716228da6e56ed6c7056aa8d3858424c28a11e
Igor <gorbov18@gmail.com>
2019-07-24 20:34:31 +0400

    Connect winston

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+const winston = require('./config/winston');
+
 const authRoutes = require('./routes/authRoutes');
 const snippetRouter = require('./routes/snippetRoutes.js');
 const categoryRouter = require('./routes/categoryRoutes');
@@ -10,6 +12,7 @@ const categoryRouter = require('./routes/categoryRoutes');
 const app = express();
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+else app.use(morgan('combined', { stream: winston.stream }));
 
 app.use(cookieParser());
 
